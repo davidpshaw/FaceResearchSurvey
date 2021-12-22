003646ad517cca9c78dd00017fad48390c586ad1
David Shaw <shawd@vmware.com>
2021-12-21 22:28:41 -0500

    - progress on the image page

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 4848
 const path = require('path')
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/images', express.static('images'))
+
 app.get('/', (req, res, next) => {
   const options = {
     root: path.join('html')
@@ -20,7 +22,20 @@ app.get('/', (req, res, next) => {
 })
 
 app.post('/', (req, res, next) => {
-  res.json({ req: JSON.stringify(req.body) })
+  const options = {
+    root: path.join('html')
+  }
+  const fileName = 'surveyImage.html'
+
+  console.log(JSON.stringify(req.body))
+
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err)
+    } else {
+      console.log('Sent:', fileName)
+    }
+  })
 })
 
 app.listen(port, () => {
