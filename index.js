472bc75e5eb0b642c3be4c631aaa362f8881ac2c
David Shaw <shawd@vmware.com>
2021-12-22 13:46:15 -0500

    - progress

--- index.js
+++ index.js
@@ -1,42 +1,123 @@
 const express = require('express')
 const app = express()
 const port = 4848
+const eta = require('eta')
 const path = require('path')
+const fs = require('fs')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const req = require('express/lib/request')
+
+const numberOfImagesPerSurvey = 20
+
+// process urlencoded form data
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/images', express.static('images'))
+// config for Eta template rendering engine
+app.engine('eta', eta.renderFile)
+app.set('view engine', 'eta')
+app.set('views', './views')
 
-app.get('/', (req, res, next) => {
-  const options = {
-    root: path.join('html')
+app.use('/images', express.static('images'))
+app.use(cookieParser())
+app.use(session({
+  secret: 'DFF06695-02F7-4484-B0B2-4F89860C75B6',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: (1000 * 60 * 60 * 24) }
+})) // session config
+
+/// /
+
+// some tools to get the images
+const loadAllImageNames = () => {
+  const directoryPath = path.join(__dirname, 'images')
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err)
+    }
+    app.allImages = files
+  })
+}
+
+const chooseRandomImagesFromArray = (originalArray, imageCount) => {
+  const dupArray = originalArray.slice()
+  const newArray = []
+
+  while (newArray.length < imageCount) {
+    const random = Math.floor(Math.random() * dupArray.length)
+    newArray.push(dupArray[random])
+    dupArray.splice(random, 1)
   }
 
-  const fileName = 'survey1.html'
-  res.sendFile(fileName, options, function (err) {
-    if (err) {
-      next(err)
-    } else {
-      console.log('Sent:', fileName)
-    }
+  return newArray
+}
+
+const addImageDataToSession = (req, imageName, attractiveness, symmetrical) => {
+  if (!req.session.surveyResponse) {
+    req.session.surveyResponse = {}
+  }
+
+  req.session.surveyResponse[imageName] = {
+    attractiveness: attractiveness,
+    symmetrical: symmetrical
+  }
+}
+
+/// /
+
+app.get('/stat', function (req, res) {
+  if (req.session.page_views) {
+    req.session.page_views++
+    res.send('You visited this page ' + req.session.page_views + ' times')
+  } else {
+    req.session.page_views = 1
+    res.send('Welcome to this page for the first time!')
+  }
+})
+
+app.get('/', (req, res, next) => {
+  // for each new session, define some survey images from the full set
+  if (!req.session.surveyImages) {
+    req.session.surveyImages = chooseRandomImagesFromArray(app.allImages, numberOfImagesPerSurvey)
+    console.log(`new ${req.session.surveyImages}`)
+  } else {
+    console.log(`existing ${req.session.surveyImages}`)
+  }
+  req.session.save()
+
+  res.render('survey1', {
+    stage: -1
   })
 })
 
 app.post('/', (req, res, next) => {
-  const options = {
-    root: path.join('html')
+  const prevStage = req.body.stage
+  const nextStage = parseInt(prevStage) + 1
+
+  if (req.body.imageName) {
+    addImageDataToSession(req, req.body.imageName, req.body.attractiveness, req.body.symmetrical)
   }
-  const fileName = 'surveyImage.html'
 
-  console.log(JSON.stringify(req.body))
+  if (req.session.surveyImages) {
+    console.log(`existing ${req.session.surveyImages}`)
+  } else {
+    console.log('failed')
+  }
+  console.log(`stage ${nextStage}`)
 
-  res.sendFile(fileName, options, function (err) {
-    if (err) {
-      next(err)
-    } else {
-      console.log('Sent:', fileName)
-    }
-  })
+  if (nextStage >= numberOfImagesPerSurvey) {
+    res.send('Survey is done')
+  } else {
+    res.render('surveyImage', {
+      stage: nextStage,
+      imageName: req.session.surveyImages[nextStage]
+    })
+  }
 })
+
+loadAllImageNames()
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
