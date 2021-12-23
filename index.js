83ac2cc0e1db41af6714b75072c78708c1926544
David Shaw <shawd@vmware.com>
2021-12-22 20:09:29 -0500

    - added start over button

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs')
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
 
-const numberOfImagesPerSurvey = 20
+const numberOfImagesPerSurvey = 2
 const surveyLogFile = 'survey.log'
 
 // process urlencoded form data
